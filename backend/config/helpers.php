fcc03db27fe8d84bf7fbca783cb728ad4cbe19a2
Anumon Jacob <anumonjacob31@gmail.com>
2026-05-26 16:23:03 +0400

    Add no-cache headers to backend API

--- backend/config/helpers.php
+++ backend/config/helpers.php
@@ -6,6 +6,9 @@ function setupCORS() {
     $origin = getenv('ALLOWED_ORIGIN') ?: '*';
     header("Access-Control-Allow-Origin: " . $origin);
     header("Content-Type: application/json; charset=UTF-8");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Authorization, X-Requested-With");
